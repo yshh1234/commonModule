2eca93603bb157bcedd02fde20835b5026637a4b
yshh1234 <yshh1234@163.com>
2016-11-21 22:26:42 +0800

    create project

---
+++ demo/js/demoForZepto.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Evonnehui on 2016/11/21.
+ */
+define(function (require,exports,module) {
+    'use strict';
+    var commonModuel = require('');
+
+    module.exports = ThisPage;
+});
