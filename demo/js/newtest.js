4104604820b418d0fb4daa6644363166d46eaf48
yshh1234 <yshh1234@163.com>
2016-12-20 11:09:17 +0800

    Update newtest.js

--- demo/js/newtest.js
+++ demo/js/newtest.js
@@ -43,5 +43,36 @@ window.onload = function () {
                 }
             });
         }
-
+        
+    /**
+     * IOS输入控件覆盖问题解决
+     * @param ele 控制的input元素
+     * @param isfixed input元素的父级/本身是否fixed
+     */
+    util.inputScrollTop = function (ele, isfixed) {
+        (function(_ele, _isfixed){
+            //当前元素
+            var _this = $(_ele);
+            if (UP.W.Env.isIOS && UP.W.Env.isInsideWalletApp) {
+                _this.on('focus', function () {
+                    //添加绑定事件
+                    setTimeout(function () {
+                        //当前body滚动标度
+                        var parentEleScrollTop = document.body.scrollTop;
+                        //需要滚动的实际标度
+                        var scroll = _this.offset().top + parseInt(_this.css('height')) - document.body.clientHeight + 318;
+                        //元素高于输入框则不滚动
+                        if (parentEleScrollTop >= scroll) {
+                            return;
+                        }
+                        //是否fixed
+                        if (_isfixed) {
+                            scroll = document.body.scrollTop + 318;
+                        }
+                        document.body.scrollTop = scroll;
+                    }, 300);
+                });
+            }
+        })(ele, isfixed);
+    }
     }
